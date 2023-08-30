b4b47c2682dc26281fd1a57cfba8393d4b58df78
Karolina Śnieżek <karolina.sniezek@xebia.com>
2023-08-30 12:18:22 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+const OpenAI = require("openai");
+
+
+app.post('/images', async (req, res) => {
+
+async function main() {
+  try {
+    const openai = new OpenAI({
+        apiKey: process.env.API_KEY,
+      });
+
+    const image = await openai.images.generate({ 
+        prompt: "A cute baby sea otter",
+        n: 2,
+        size: "1024x1024"
+     });
+        res.send(image.data)
+        console.log(image.data);
+  } catch (error) {
+    console.error(error)
+  }
+}
+main();
+})
+
+app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
