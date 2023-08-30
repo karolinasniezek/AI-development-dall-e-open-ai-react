b8f3946ddf223daeba6e93e34172fb2daf77c359
Karolina Śnieżek <karolina.sniezek@xebia.com>
2023-08-30 22:11:06 +0200

    add generate variations to check size method

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react"
 
-const Modal = ({setModalOpen, setSelectedImage, selectedImage}) => {
+const Modal = ({setModalOpen, setSelectedImage, selectedImage, generateVariations}) => {
     const [error, setError] = useState(null)
     const ref = useRef(null)
 
@@ -12,7 +12,7 @@ const Modal = ({setModalOpen, setSelectedImage, selectedImage}) => {
 
     const checkSize = () => {
         if (ref.current.width == 256 && ref.current.height == 256) {
-            // generateVariations()
+            generateVariations()
         } else {
             setError('Error: Choose 256 x 256 image')
         }
