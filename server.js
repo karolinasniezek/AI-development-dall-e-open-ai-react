e06bc96fb00cedcc6ad818bdd309132a38a91fba
Karolina Śnieżek <karolina.sniezek@xebia.com>
2023-08-30 17:23:13 +0200

    add upload image request

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 const OpenAI = require("openai");
+const fs = require('fs')
+const multer = require('multer')
 
-
+const storage  = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public')
+    },
+    filename: (req, file, cb) => {
+        console.log('file', file)
+        cb(null, Date.now() + "-" + file.originalname)
+    }
+})
+const upload = multer({storage: storage}).single('file')
 app.post('/images', async (req, res) => {
 
 async function main() {
@@ -28,6 +39,17 @@ async function main() {
   }
 }
 main();
+})
+
+app.post("/upload", (req, res) => {
+    upload(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        } else if (err) {
+            return res.status(500).json(err)
+        }
+        console.log(req.file)
+    })
 })
 
 app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
